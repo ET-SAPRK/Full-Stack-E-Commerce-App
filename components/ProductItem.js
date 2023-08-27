9c51dab87dba8fce8b84e6ba6414f84137ba0c80
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-27 15:21:15 +0300

    edited

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Pressable, Image } from "react-native";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/CartReducer";
 
-const ProductItem = () => {
+const ProductItem = ({ item }) => {
+  const [addedToCart, setAddedToCart] = useState(false);
   return (
-    <View>
-      <Text>ProductItem</Text>
-    </View>
-  )
-}
+    <Pressable style={{ marginHorizontal: 20, marginVertical: 25 }}>
+      <Image
+        style={{ width: 250, height: 250, resizeMode: "contain" }}
+        source={{ uri: item?.image }}
+      />
+      <Text numberOfLines={1} style={{ width: 150, marginTop: 10 }}>
+        {item?.title}
+      </Text>
+      <View
+        style={{
+          marginTop: 5,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+          {item?.price} Br
+        </Text>
+        <Text style={{ color: "#FFC72C", fontWeight: "bold" }}>
+          {item?.rating?.rate} ratings
+        </Text>
+      </View>
 
-export default ProductItem
+      <Pressable
+        onPress={() => {}}
+        style={{
+          backgroundColor: "#FFC72C",
+          padding: 10,
+          borderRadius: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          marginHorizontal: 10,
+          marginTop: 10,
+        }}
+      >
+        {addedToCart ? (
+          <View>
+            <Text>Added to Cart</Text>
+          </View>
+        ) : (
+          <Text>Add to Cart</Text>
+        )}
+      </Pressable>
+    </Pressable>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default ProductItem;
+
+const styles = StyleSheet.create({});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -204,6 +204,20 @@ const HomeScreen = () => {
       size: "8GB RAM, 128GB Storage",
     },
   ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.log("error message", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <SafeAreaView
@@ -333,6 +347,88 @@ const HomeScreen = () => {
                   source={{ uri: item?.image }}
                 />
               </Pressable>
+            ))}
+          </View>
+          <Text
+            style={{
+              height: 1,
+              borderColor: "#D0D0D0",
+              borderWidth: 2,
+              marginTop: 15,
+            }}
+          />
+
+          <Text style={{ padding: 10, fontSize: 18, fontWeight: "bold" }}>
+            Today's Deals
+          </Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {offers.map((item, idex) => (
+              <Pressable
+                onPress={() =>
+                  navigation.navigate("Info", {
+                    id: item.id,
+                    title: item.title,
+                    price: item?.price,
+                    carouselImages: item.carouselImages,
+                    color: item?.color,
+                    size: item?.size,
+                    oldPrice: item?.oldPrice,
+                    item: item,
+                  })
+                }
+                style={{
+                  marginVertical: 10,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  style={{ width: 150, height: 150, resizeMode: "contain" }}
+                  source={{ uri: item?.image }}
+                />
+                <View
+                  style={{
+                    backgroundColor: "#E31837",
+                    paddingVertical: 5,
+                    width: 130,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop: 10,
+                    borderRadius: 4,
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 13,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Upto {item?.offer}
+                  </Text>
+                </View>
+              </Pressable>
+            ))}
+          </ScrollView>
+          <Text
+            style={{
+              height: 1,
+              borderColor: "#D0D0D0",
+              borderWidth: 2,
+              marginTop: 15,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            {products?.map((item, index) => (
+              <ProductItem item={item} key={index} />
             ))}
           </View>
         </ScrollView>
