bffa0486ea842fb9232fb161032d2a228fb809ee
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-29 23:22:07 +0300

    edited

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import LottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
 const OrderScreen = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace("Main");
+    }, 1300);
+  }, []);
   return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
+      <Text
+        style={{
+          marginTop: 20,
+          fontSize: 19,
+          fontWeight: "600",
+          textAlign: "center",
+        }}
+      >
+        Your Order Has been Recieved
+      </Text>
+    </SafeAreaView>
+  );
+};
 
-export default OrderScreen
+export default OrderScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
