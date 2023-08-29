e46340dd40a7dbbbd1b358e8eb6fec0d77971765
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-29 18:53:16 +0300

    edited

--- api/index.js
+++ api/index.js
@@ -34,38 +34,38 @@ const User = require("./models/user");
 const Order = require("./models/order");
 
 const sendVerificationEmail = async (email, verificationToken) => {
-    // Create a Nodemailer transporter
-    const transporter = nodemailer.createTransport({
-      // Configure the email service or SMTP details here
-      service: "gmail",
-      auth: {
-        user: process.env.APP_USER,
-        pass: process.env.APP_PASS,
-      },
-    });
-    const mailOptions = {
-        from: "amazon.com",
-        to: email,
-        subject: "Email Verification",
-        text: `Please click the following link to verify your email: http://192.168.43.207:8000/verify/${verificationToken}`,
-      };
-      try {
-        await transporter.sendMail(mailOptions);
-        console.log("Verification email sent successfully");
-      } catch (error) {
-        console.error("Error sending verification email:", error);
-      }
-    };  
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    // Configure the email service or SMTP details here
+    service: "gmail",
+    auth: {
+      user: process.env.APP_USER,
+      pass: process.env.APP_PASS,
+    },
+  });
+  const mailOptions = {
+    from: "amazon.com",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click the following link to verify your email: http://192.168.43.207:8000/verify/${verificationToken}`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent successfully");
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  }
+};
 
 app.post("/register", async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const existingUser = await User.findOne({ email })
-        if (existingUser) {
-            console.log("Email already registered:", email); // Debugging statement
-            return res.status(400).json({ message: "Email already registered" });
-          }
-          const newUser = new User({ name, email, password });
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email already registered:", email); // Debugging statement
+      return res.status(400).json({ message: "Email already registered" });
+    }
+    const newUser = new User({ name, email, password });
 
     // Generate and store the verification token
     newUser.verificationToken = crypto.randomBytes(20).toString("hex");
@@ -75,58 +75,95 @@ app.post("/register", async (req, res) => {
     res.status(201).json({
       message:
         "Registration successful. Please check your email for verification.",
-    });  
-    } catch (error) {
-        console.log("Error during registration:", error); // Debugging statement
-        res.status(500).json({ message: "Registration failed" });
-    }
-})
+    });
+  } catch (error) {
+    console.log("Error during registration:", error); // Debugging statement
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
 
 app.get("/verify/:token", async (req, res) => {
-    try {
-        const token = req.params.token;
-        const user = await User.findOne({ verificationToken: token });
-        if (!user) {
-            return res.status(404).json({ message: "Invalid verification token" });
-          }
-          user.verified = true;
-          user.verificationToken = undefined; 
-          
-          await user.save(); 
-          res.status(200).json({ message: "Email verified successfully" });
-    } catch(error) {
-        res.status(500).json({ message: "Email Verificatioion Failed" });
+  try {
+    const token = req.params.token;
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid verification token" });
     }
-})
+    user.verified = true;
+    user.verificationToken = undefined;
+
+    await user.save();
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Email Verificatioion Failed" });
+  }
+});
 
 const generateSecretKey = () => {
-    const secretKey = crypto.randomBytes(32).toString("hex");
-  
-    return secretKey;
-  };
-  
-  const secretKey = generateSecretKey();
+  const secretKey = crypto.randomBytes(32).toString("hex");
+
+  return secretKey;
+};
+
+const secretKey = generateSecretKey();
 
 app.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      //check if the user exists
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-  
-      //check if the password is correct
-      if (user.password !== password) {
-        return res.status(401).json({ message: "Invalid password" });
-      }
-  
-      //generate a token
-      const token = jwt.sign({ userId: user._id }, secretKey);
-  
-      res.status(200).json({ token });
-    } catch (error) {
-      res.status(500).json({ message: "Login Failed" });
+  try {
+    const { email, password } = req.body;
+
+    //check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-  });
+
+    //check if the password is correct
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    //generate a token
+    const token = jwt.sign({ userId: user._id }, secretKey);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login Failed" });
+  }
+});
+app.post("/addresses", async (req, res) => {
+  try {
+    const { userId, address } = req.body;
+
+    //find the user by the Userid
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    //add the new address to the user's addresses array
+    user.addresses.push(address);
+
+    //save the updated user in te backend
+    await user.save();
+
+    res.status(200).json({ message: "Address created Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error addding address" });
+  }
+});
+
+app.get("/addresses/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const addresses = user.addresses;
+    res.status(200).json({ addresses });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieveing the addresses" });
+  }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -34,6 +34,7 @@ const HomeScreen = () => {
   const [category, setCategory] = useState("jewelery");
   // const { userId, setUserId } = useContext(UserType);
   const [selectedAddress, setSelectedAdress] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
   const [items, setItems] = useState([
     { label: "Men's clothing", value: "men's clothing" },
     { label: "jewelery", value: "jewelery" },
@@ -227,7 +228,6 @@ const HomeScreen = () => {
 
     fetchData();
   }, []);
-
   return (
     <>
       <SafeAreaView
@@ -474,6 +474,90 @@ const HomeScreen = () => {
           </View>
         </ScrollView>
       </SafeAreaView>
+      <BottomModal
+        onBackdropPress={() => setModalVisible(!modalVisible)}
+        swipeDirection={["up", "down"]}
+        swipeThreshold={200}
+        modalAnimation={
+          new SlideAnimation({
+            slideFrom: "bottom",
+          })
+        }
+        onHardwareBackPress={() => setModalVisible(!modalVisible)}
+        visible={modalVisible}
+        onTouchOutside={() => setModalVisible(!modalVisible)}
+      >
+        <ModalContent style={{ width: "100%", height: 400 }}>
+          <View style={{ marginBottom: 8 }}>
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>
+              Choose your Location
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 16, color: "gray" }}>
+              Select a delivery location to see product availabilty and delivery
+              options
+            </Text>
+          </View>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {/* {addresses?.map((item, index) => ( */}
+            <Pressable
+              onPress={() => {
+                setModalVisible(false);
+                navigation.navigate("Address");
+              }}
+              style={{
+                width: 140,
+                height: 140,
+                borderColor: "#D0D0D0",
+                borderWidth: 1,
+                padding: 10,
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 3,
+                marginRight: 15,
+                marginTop: 10,
+                backgroundColor: "#FBCEB1",
+              }}
+            >
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 3 }}
+              >
+                <Text style={{ fontSize: 13, fontWeight: "bold" }}>
+                  Add address
+                </Text>
+                <Entypo name="location-pin" size={24} color="red" />
+              </View>
+            </Pressable>
+            {/* ))} */}
+          </ScrollView>
+          <View style={{ flexDirection: "column", gap: 7, marginBottom: 30 }}>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <Entypo name="location-pin" size={22} color="#0066b2" />
+              <Text style={{ color: "#0066b2", fontWeight: "400" }}>
+                Enter an Ethiopian pincode
+              </Text>
+            </View>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <Ionicons name="locate-sharp" size={22} color="#0066b2" />
+              <Text style={{ color: "#0066b2", fontWeight: "400" }}>
+                Use My Currect location
+              </Text>
+            </View>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <AntDesign name="earth" size={22} color="#0066b2" />
+
+              <Text style={{ color: "#0066b2", fontWeight: "400" }}>
+                Deliver outside Ethiopia
+              </Text>
+            </View>
+          </View>
+        </ModalContent>
+      </BottomModal>
     </>
   );
 };
